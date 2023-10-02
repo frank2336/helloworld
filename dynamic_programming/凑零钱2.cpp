4912928b5085fb461ed18e5a6049bcf5f9a3e0e4
Frank Wang <frank2336@gmail.com>
2023-10-02 10:57:33 +0800

    Create 凑零钱2.cpp
    动态规划，凑零钱，小码王例题
    https://www.luogu.com.cn/problem/T244965

---
+++ dynamic_programming/凑零钱2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int N = 1e6 + 10; // 如果赋值1e10+10，在洛谷提交代码时，编译就会失败
+int f[N];
+
+int main(void) {
+    int n = 0;
+
+    cin >> n;
+
+    for (int i=1; i<=n; i++) {
+        if (i==1) {
+            f[i] = 1;
+        } else if (i < 5) {
+            f[i] = min(f[i-1], f[i-2]) + 1;
+        } else if (i>=5) {
+            f[i] = min(min(f[i-1], f[i-2]), f[i-5]) + 1;
+        }
+    }
+    cout << f[n] << endl;
+    return 0;
+}
