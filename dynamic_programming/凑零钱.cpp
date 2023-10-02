b3ec79e1f72051d2febaa64e4f11aa7a57c478e4
Frank Wang <frank2336@gmail.com>
2023-10-02 10:02:11 +0800

    Create 凑零钱.cpp

---
+++ dynamic_programming/凑零钱.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1e5 + 10;
+int f[N];
+
+int main(void)
+{
+    int n = 0;
+
+    cin >> n;
+    for (int i=1; i<=n; i++) {
+        if (i<5) {
+            f[i] = f[i-1] + 1;
+        } else if (i<11) {
+            f[i] = min(f[i-1], f[i-5]) + 1;
+        } else {
+            f[i] = min(min(f[i-1], f[i-5]), f[i-11]) + 1;
+        }
+    }
+    cout << f[n] << endl;
+    return 0;
+}
+
